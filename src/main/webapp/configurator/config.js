1abcb9302871820df5d20f7829ea78d92bb1dcbc
Evandro Carenho <ecarrenho@daitangroup.com>
2017-03-16 12:19:54 -0300

    Adjusting configurator app settings.

--- src/main/webapp/configurator/config.js
+++ src/main/webapp/configurator/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	app_title: "Configure Universal Webhook Integration",
-	app_name: "Universal Webhook",
-	IM_shorthand: "UWHIB", // will display "One on One with UWHIB"
+	app_title: "Configure HubSpot Integration",
+	app_name: "HubSpot",
+	IM_shorthand: "HubSpot", // will display "One on One with UWHIB"
 	toogleSetupInstructions: false
 }
